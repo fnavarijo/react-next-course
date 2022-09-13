b46b2e2e75bad85a169488d5abf852d3bcca2512
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-09-13 00:08:25 -0600

    Handling form

--- admin-app/pages/login.js
+++ admin-app/pages/login.js
@@ -1,24 +1,36 @@
+import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 import styles from '../styles/pages/Login.module.css';
 
 function Login () {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const doLogin = (e) => {
+    e.preventDefault();
+    console.log('Hello', e, username, password);
+  }
+
+  const onUserNameChange = (e) => setUsername(e.target.value);
+  const onPasswordNameChange = (e) => setPassword(e.target.value);
+
   return <div className={styles.loginContainer}>
-    <Form>
+    <Form onSubmit={doLogin}>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label>Usuario</Form.Label>
-        <Form.Control type="text" placeholder="Usuario" />
+        <Form.Control type="text" placeholder="Usuario" value={username} onChange={onUserNameChange} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label>Contraseña</Form.Label>
-        <Form.Control type="password" placeholder="Contraseña" />
+        <Form.Control type="password" placeholder="Contraseña" value={password} onChange={onPasswordNameChange} />
       </Form.Group>
+      <div className={styles.loginButtons}>
+        <Button type="submit">Iniciar Sesion</Button>
+        <Button variant="outline-primary">Crear cuenta</Button>
+      </div>
     </Form>
-    <div className={styles.loginButtons}>
-      <Button>Iniciar Sesion</Button>
-      <Button variant="outline-primary">Crear cuenta</Button>
-    </div>
   </div>
 }
 
