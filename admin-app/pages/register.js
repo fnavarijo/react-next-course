2e3365b310db379de5fc81e09e8ccb457a37f68a
Fernando Navarijo <fernandonavarijo1996@gmail.com>
2022-09-13 20:43:06 -0600

    Adding basic form of validation

--- admin-app/pages/register.js
+++ admin-app/pages/register.js
@@ -5,13 +5,20 @@ import Button from "react-bootstrap/Button";
 import styles from "../styles/pages/Login.module.css";
 
 function Register() {
+  const [validated, setValidated] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
   const doLogin = (e) => {
     e.preventDefault();
-    console.log("Hello", username, password);
+    const form = e.currentTarget;
+
+    if (form.checkValidity()) {
+      console.log('Is working')
+    }
+
+    setValidated(true);
   };
 
   const onUserNameChange = (e) => setUsername(e.target.value);
@@ -20,33 +27,42 @@ function Register() {
 
   return (
     <div className={styles.loginContainer}>
-      <Form onSubmit={doLogin}>
+      <Form noValidate validated={validated} onSubmit={doLogin}>
         <Form.Group className="mb-3">
           <Form.Label>Usuario</Form.Label>
           <Form.Control
             type="text"
             placeholder="Usuario"
+            required
             value={username}
             onChange={onUserNameChange}
           />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          <Form.Control.Feedback type="invalid">Not cool!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Contraseña</Form.Label>
           <Form.Control
             type="password"
             placeholder="Contraseña"
+            required
+            minLength={6}
             value={password}
             onChange={onPasswordNameChange}
           />
+          <Form.Control.Feedback type="invalid">Ingrese contraseña valida!</Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>Contraseña</Form.Label>
           <Form.Control
             type="password"
             placeholder="Contraseña"
+            required
+            minLength={6}
             value={confirmPassword}
             onChange={onConfirmPasswordNameChange}
           />
+          <Form.Control.Feedback type="invalid">Ingrese contraseña valida!</Form.Control.Feedback>
         </Form.Group>
         <div className={styles.loginButtons}>
           <Button type="submit">Registrar</Button>
